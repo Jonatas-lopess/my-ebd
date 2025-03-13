0af47d7d7bdfee33cd0c4d8f83358eaf4ba59a13
jonatas-lopess <jonatinhalopes.011@gmail.com>
2025-03-13 19:08:17 -0300

    feat: create CustomCard component | HomeScreen (In Progress)

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -4,53 +4,70 @@ import ThemedText from "../../components/ThemedText";
 import { useTheme } from "@shopify/restyle";
 import { ThemeProps } from "../../theme";
 import { ScrollView } from "react-native";
+import CustomCard from "../../components/CustomCard";
 
 export default function HomeScreen() {
-  const iconSize = useTheme<ThemeProps>().spacing.l;
+  const theme = useTheme<ThemeProps>();
+  const iconSize = theme.spacing.l;
 
   return (
-    <ThemedView flex={1}>
-      <ThemedView height="45%" backgroundColor="primary" paddingTop="safeArea">
-        <ThemedView flexDirection="row" mx="s" mt="s">
-          <ThemedView flex={1} alignItems="center" pl="l">
-            <ThemedText color="gray" variant="body" fontWeight="bold">
-              Minha EBD
+    <ThemedView flex={1} backgroundColor="secondary">
+      <ThemedView paddingTop="safeArea">
+        <ScrollView>
+          <ThemedView flexDirection="row" mx="s" mt="s">
+            <ThemedView flex={1} alignItems="center" pl="l">
+              <ThemedText color="gray" variant="body" fontWeight="bold">
+                Minha EBD
+              </ThemedText>
+            </ThemedView>
+            <Ionicons name="menu" size={iconSize} color="white" />
+          </ThemedView>
+
+          <ThemedView flexDirection="column" alignItems="center" mt="m">
+            <ThemedText variant="header" color="white">
+              Escola Bíblica
+            </ThemedText>
+            <ThemedText variant="body" color="white">
+              Vila Mury
             </ThemedText>
           </ThemedView>
-          <Ionicons name="menu" size={iconSize} color="white" />
-        </ThemedView>
 
-        <ThemedView flexDirection="column" alignItems="center" mt="m">
-          <ThemedText variant="header" color="white">
-            Escola Bíblica
-          </ThemedText>
-          <ThemedText variant="body" color="white">
-            Vila Mury
-          </ThemedText>
-        </ThemedView>
+          <ScrollView
+            horizontal={true}
+            style={{ marginTop: 20 }}
+            contentContainerStyle={{ gap: 10, paddingHorizontal: 10 }}
+          >
+            <CustomCard
+              text="Últimas 13 aulas"
+              height={theme.spacing.xl}
+              active
+            />
+            <CustomCard
+              text="2º Trimestre"
+              height={theme.spacing.xl}
+              onPress={() => alert("oi")}
+            />
+            <CustomCard text="1º Trimestre" height={theme.spacing.xl} />
+          </ScrollView>
 
-        <ScrollView horizontal={true} style={{ marginTop: 10 }}>
+          <ThemedView flexDirection="row" mt="xl" justifyContent="space-around">
+            <CustomCard text="Matriculados: 22" height={34} active={false} />
+            <CustomCard
+              text="Média no Intervalo: 76%"
+              height={34}
+              active={false}
+            />
+          </ThemedView>
+
           <ThemedView
-            width={150}
-            height={30}
+            mt="s"
+            height="auto"
             backgroundColor="white"
-            borderRadius={10}
-            mx="s"
-          />
-          <ThemedView
-            width={150}
-            height={30}
-            backgroundColor="white"
-            borderRadius={10}
-            mx="s"
-          />
-          <ThemedView
-            width={150}
-            height={30}
-            backgroundColor="white"
-            borderRadius={10}
-            mx="s"
-          />
+            borderTopLeftRadius={20}
+            borderTopRightRadius={20}
+          >
+            <ThemedView height={450} />
+          </ThemedView>
         </ScrollView>
       </ThemedView>
     </ThemedView>
