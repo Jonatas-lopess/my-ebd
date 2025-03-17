a8125cc6d5455e087a857b26f07f4237bc70e26e
jonatas-lopess <jonatinhalopes.011@gmail.com>
2025-03-17 17:48:05 -0300

    StudentScreen in progress

--- src/screens/StudentScreen/index.tsx
+++ src/screens/StudentScreen/index.tsx
@@ -3,18 +3,60 @@ import { ThemeProps } from "../../theme";
 import ThemedText from "../../components/ThemedText";
 import ThemedView from "../../components/ThemedView";
 import FocusAwareStatusBar from "../../components/FocusAwareStatusBar";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { useState } from "react";
+import { TextInput, View } from "react-native";
 
 export default function StudentScreen() {
   const theme = useTheme<ThemeProps>();
+  const [birthdayFilter, setBirthdayFilter] = useState(false);
+  const [nameFilter, setNameFilter] = useState("");
 
   return (
     <ThemedView flex={1} backgroundColor="white">
       <FocusAwareStatusBar style="dark" translucent />
 
-      <ThemedView paddingTop="safeArea">
-        <ThemedText color="gray" variant="body" fontWeight="bold">
-          Minha EBD
-        </ThemedText>
+      <ThemedView pt="safeArea" px="s">
+        <View style={{ backgroundColor: "#fff" }}>
+          <ThemedView
+            flexDirection="row"
+            alignItems="center"
+            justifyContent="space-between"
+          >
+            <ThemedView flexDirection="row" alignItems="center">
+              <ThemedText
+                color="secondary"
+                fontSize={26}
+                fontWeight="bold"
+                pr="s"
+              >
+                Minha EBD
+              </ThemedText>
+              <ThemedText color="gray" fontSize={20}>
+                • Vila Mury
+              </ThemedText>
+            </ThemedView>
+            <FontAwesome.Button
+              name="birthday-cake"
+              color={birthdayFilter ? theme.colors.primary : theme.colors.gray}
+              onPress={() => setBirthdayFilter(!birthdayFilter)}
+              backgroundColor="transparent"
+              underlayColor="transparent"
+            />
+          </ThemedView>
+
+          <TextInput
+            placeholder="Pesquisar por nome"
+            value={nameFilter}
+            onChangeText={setNameFilter}
+            style={{
+              backgroundColor: "#fff",
+              borderRadius: 20,
+              padding: theme.spacing.s,
+              marginTop: theme.spacing.l,
+            }}
+          />
+        </View>
       </ThemedView>
     </ThemedView>
   );
