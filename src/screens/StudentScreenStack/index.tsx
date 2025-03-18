5f7c1de8e9c5b0472b1ad3e54a35b0800c111394
Jonatas-lopess <jonataslopes.011@gmail.com>
2025-03-18 16:43:43 -0300

    HistoryScreen (in progress)

--- src/screens/StudentScreenStack/index.tsx
+++ src/screens/StudentScreenStack/index.tsx
@@ -7,12 +7,11 @@ export default function StudentScreenStack() {
   const Stack = createNativeStackNavigator<StudentStackParamList>();
 
   return (
-    <Stack.Navigator initialRouteName="Alunos_Lista">
-      <Stack.Screen
-        name="Alunos_Lista"
-        component={StudentScreen}
-        options={{ headerShown: false }}
-      />
+    <Stack.Navigator
+      initialRouteName="Alunos_Lista"
+      screenOptions={{ headerShown: false }}
+    >
+      <Stack.Screen name="Alunos_Lista" component={StudentScreen} />
       <Stack.Screen name="Alunos_Historico" component={HistoryScreen} />
     </Stack.Navigator>
   );
