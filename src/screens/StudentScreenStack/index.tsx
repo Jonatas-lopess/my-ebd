a86fbb5e0517cb5859ce896a804a97e0c6403d24
jonatas-lopess <jonatinhalopes.011@gmail.com>
2025-03-20 13:49:28 -0300

    refactor: StudentScreenStack from dynamic to static

--- src/screens/StudentScreenStack/index.tsx
+++ src/screens/StudentScreenStack/index.tsx
@@ -2,17 +2,22 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import StudentScreen from "./StudentScreen";
 import HistoryScreen from "./HistoryScreen";
 import { StudentStackParamList } from "../../types/navigation";
+import { createComponentForStaticNavigation } from "@react-navigation/native";
+
+const StackConfig = createNativeStackNavigator<StudentStackParamList>({
+  initialRouteName: "Alunos_Lista",
+  screens: {
+    Alunos_Lista: StudentScreen,
+    Alunos_Historico: HistoryScreen,
+  },
+  screenOptions: { headerShown: false },
+});
+
+const StudentNavigator = createComponentForStaticNavigation(
+  StackConfig,
+  "Alunos"
+);
 
 export default function StudentScreenStack() {
-  const Stack = createNativeStackNavigator<StudentStackParamList>();
-
-  return (
-    <Stack.Navigator
-      initialRouteName="Alunos_Lista"
-      screenOptions={{ headerShown: false }}
-    >
-      <Stack.Screen name="Alunos_Lista" component={StudentScreen} />
-      <Stack.Screen name="Alunos_Historico" component={HistoryScreen} />
-    </Stack.Navigator>
-  );
+  return <StudentNavigator />;
 }
