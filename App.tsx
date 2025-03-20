62c28f524d8818eedf5d4524e090ece0f47cc9d9
Jonatas-lopess <jonataslopes.011@gmail.com>
2025-03-20 12:27:38 -0300

    feat: add AuthProvider | role based routing

--- App.tsx
+++ App.tsx
@@ -1,36 +1,14 @@
 import { ThemeProvider } from "@shopify/restyle";
 import { theme } from "./src/theme";
-import HomeScreen from "./src/screens/HomeScreen";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { createStaticNavigation } from "@react-navigation/native";
-import { Ionicons } from "@expo/vector-icons";
-import StudentScreenStack from "./src/screens/StudentScreenStack";
-
-const Tabs = createBottomTabNavigator({
-  initialRouteName: "Inicio",
-  screens: {
-    Inicio: HomeScreen,
-    Alunos: StudentScreenStack,
-  },
-  screenOptions: ({ route }) => ({
-    headerShown: false,
-    tabBarIcon: ({ color, size }) => {
-      let icon: keyof typeof Ionicons.glyphMap = "tablet-landscape-outline";
-
-      if (route.name === "Inicio") icon = "home";
-      if (route.name === "Alunos") icon = "people";
-
-      return <Ionicons name={icon} size={size} color={color} />;
-    },
-  }),
-});
-
-const TabNavigation = createStaticNavigation(Tabs);
+import RootTabNavigator from "./src/components/RootTabNavigator";
+import AuthProvider from "./src/providers/AuthProvider";
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
-      <TabNavigation />
+      <AuthProvider>
+        <RootTabNavigator />
+      </AuthProvider>
     </ThemeProvider>
   );
 }
