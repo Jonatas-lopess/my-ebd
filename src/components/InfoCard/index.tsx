c5bfe0b6cb9537bfb5c8dc3714f9b9c293b3ab1a
Jonatas-lopess <jonataslopes.011@gmail.com>
2025-03-21 11:15:05 -0300

    feat: add InfoCard component

---
+++ src/components/InfoCard/index.tsx
@@ -0,0 +1,71 @@
+import ThemedText from "../ThemedText";
+import ThemedView from "../ThemedView";
+import { Pressable } from "react-native";
+
+type Info = {
+  title: string;
+  detail: string;
+};
+
+type BaseInfoCardProps = {
+  title: string;
+  detail?: string;
+};
+
+type PropsWithInfo = BaseInfoCardProps & {
+  info: Info;
+};
+
+type PropsWithExtraInfo = BaseInfoCardProps & {
+  extraInfo: Info;
+};
+
+type InfoCardProps = PropsWithInfo | PropsWithExtraInfo;
+
+export default function InfoCard({ detail, title, ...rest }: InfoCardProps) {
+  const { info } = rest as PropsWithInfo;
+  const { extraInfo } = rest as PropsWithExtraInfo;
+
+  return (
+    <Pressable onPress={() => {}} onLongPress={() => alert("long press")}>
+      <ThemedView
+        style={{ backgroundColor: "white" }}
+        borderRadius={10}
+        py="s"
+        px="m"
+      >
+        <ThemedView alignItems="center" flexDirection="row" mb="s">
+          <ThemedText variant="h3" mr="xs">
+            {title}
+          </ThemedText>
+          {detail && (
+            <ThemedText variant="body" color="gray">
+              • {detail}
+            </ThemedText>
+          )}
+        </ThemedView>
+        <ThemedView
+          borderTopWidth={1}
+          borderColor="lightgrey"
+          flexDirection="row"
+          justifyContent="space-around"
+          pt="s"
+        >
+          {info && (
+            <ThemedText>
+              {info.title}: {info.detail}
+            </ThemedText>
+          )}
+          {extraInfo && (
+            <>
+              <ThemedView borderLeftWidth={1} borderLeftColor="lightgrey" />
+              <ThemedText>
+                {extraInfo.title}: {extraInfo.detail}
+              </ThemedText>
+            </>
+          )}
+        </ThemedView>
+      </ThemedView>
+    </Pressable>
+  );
+}
