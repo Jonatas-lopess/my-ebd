857fa1aa69c2b73b83469cfa1410926b33fbf4a3
Jonatas-lopess <jonataslopes.011@gmail.com>
2025-04-04 16:16:51 -0300

    fix: TextButton and NumberPick components | gesturehandler view

--- App.tsx
+++ App.tsx
@@ -2,13 +2,16 @@ import { ThemeProvider } from "@shopify/restyle";
 import { theme } from "./src/theme";
 import AuthProvider from "./src/providers/AuthProvider";
 import AuthController from "./src/components/AuthController";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <AuthProvider>
-        <AuthController />
-      </AuthProvider>
-    </ThemeProvider>
+    <AuthProvider>
+      <ThemeProvider theme={theme}>
+        <GestureHandlerRootView>
+          <AuthController />
+        </GestureHandlerRootView>
+      </ThemeProvider>
+    </AuthProvider>
   );
 }
