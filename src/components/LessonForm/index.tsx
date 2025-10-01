efe9b4912bd5f685fb25792eec568259b1be549b
Jonatas-lopess <jonataslopes.011@gmail.com>
2025-10-01 11:44:05 -0300

    feat(LessonScreen): Modal bug fixes

---
+++ src/components/LessonForm/index.tsx
@@ -0,0 +1,178 @@
+import { CustomBottomModal } from "@components/CustomBottomModal";
+import ThemedText from "@components/ThemedText";
+import ThemedView from "@components/ThemedView";
+import { useEffect, useState } from "react";
+import { Ionicons } from "@expo/vector-icons";
+import {
+  DateTimePickerAndroid,
+  DateTimePickerEvent,
+} from "@react-native-community/datetimepicker";
+import { useAuth } from "@providers/AuthProvider";
+import { Lesson } from "@screens/LessonStack/HomeScreen/type";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import config from "config";
+import { theme } from "@theme";
+import { Alert } from "react-native/Libraries/Alert/Alert";
+
+type Props = {
+  mutateFallback: React.Dispatch<React.SetStateAction<boolean>>;
+  defaultLessonNumber: number | undefined;
+};
+
+export default function LessonForm({
+  mutateFallback,
+  defaultLessonNumber,
+}: Props) {
+  const today = new Date();
+  const queryClient = useQueryClient();
+  const { user, token } = useAuth().authState;
+  const DEFAULTLESSON: Lesson = {
+    flag: user?.plan ?? "",
+    number: defaultLessonNumber,
+    date: today,
+  };
+  const [newLesson, setNewLesson] = useState<Lesson>(DEFAULTLESSON);
+
+  const isLessonNumberInvalid =
+    newLesson.number === undefined ||
+    newLesson.number === defaultLessonNumber ||
+    newLesson.number === 1;
+
+  const { isPending } = useMutation({
+    mutationFn: async () => {
+      const response = await fetch(config.apiBaseUrl + "/lessons", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify(newLesson),
+      });
+
+      const data = await response.json();
+      if (!response.ok) throw new Error(data.message, { cause: data });
+
+      return data;
+    },
+    onSuccess: () => {
+      return queryClient.invalidateQueries({ queryKey: ["lessons"] });
+    },
+    onError: (error) => {
+      Alert.alert(
+        "Algo deu errado!",
+        "Não foi possível criar a aula. Tente novamente mais tarde."
+      );
+
+      console.log(error.message, error.cause);
+    },
+  });
+
+  const handleNewLessonDateChange = (
+    e: DateTimePickerEvent,
+    selectedDate?: Date
+  ) =>
+    e.type === "set" &&
+    setNewLesson((oldLesson) => {
+      return {
+        ...oldLesson,
+        date: selectedDate ?? oldLesson.date,
+      };
+    });
+
+  const handleCreateNewLesson = () => {
+    //mutate();
+    console.log(newLesson);
+  };
+
+  useEffect(() => {
+    mutateFallback(isPending);
+  }, [isPending]);
+
+  return (
+    <ThemedView g="s">
+      <ThemedView
+        py="xs"
+        px="m"
+        flexDirection="row"
+        justifyContent="space-between"
+        alignItems="center"
+        borderRadius={25}
+        borderWidth={1}
+        borderColor="lightgrey"
+      >
+        <ThemedText fontSize={16} style={{ color: "grey" }}>
+          Data da aula
+        </ThemedText>
+        <ThemedText
+          onPress={() =>
+            DateTimePickerAndroid.open({
+              value: today,
+              minimumDate: today,
+              onChange: handleNewLessonDateChange,
+            })
+          }
+          fontSize={16}
+          fontWeight="600"
+        >
+          {newLesson.date.toLocaleString("pt-BR", {
+            day: "2-digit",
+            month: "2-digit",
+            year: "numeric",
+          })}
+        </ThemedText>
+      </ThemedView>
+      <ThemedView
+        py="xs"
+        px="m"
+        flexDirection="row"
+        justifyContent="space-between"
+        alignItems="center"
+        borderRadius={25}
+        borderWidth={1}
+        borderColor="lightgrey"
+      >
+        <ThemedText fontSize={16} style={{ color: "grey" }}>
+          Número da lição
+        </ThemedText>
+        <ThemedView flexDirection="row" gap="s">
+          <Ionicons
+            name="arrow-up"
+            size={25}
+            style={{ margin: 0 }}
+            onPress={() =>
+              setNewLesson((prev) => ({
+                ...prev,
+                number:
+                  prev.number === undefined
+                    ? defaultLessonNumber! + 1
+                    : prev.number + 1,
+              }))
+            }
+          />
+          <ThemedText fontSize={16} fontWeight="600">
+            {newLesson.number ?? defaultLessonNumber}
+          </ThemedText>
+          <Ionicons
+            name="arrow-down"
+            size={25}
+            style={{ margin: 0 }}
+            onPress={() => {
+              if (isLessonNumberInvalid) return;
+
+              setNewLesson((prev) => ({
+                ...prev,
+                number:
+                  prev.number === undefined
+                    ? defaultLessonNumber! - 1
+                    : prev.number - 1,
+              }));
+            }}
+            color={isLessonNumberInvalid ? theme.colors.lightgrey : "black"}
+          />
+        </ThemedView>
+      </ThemedView>
+
+      <CustomBottomModal.Action onPress={handleCreateNewLesson} />
+    </ThemedView>
+  );
+}
--- src/screens/LessonStack/HomeScreen/index.tsx
+++ src/screens/LessonStack/HomeScreen/index.tsx
@@ -3,37 +3,30 @@ import ThemedText from "@components/ThemedText";
 import ThemedView from "@components/ThemedView";
 import { ThemeProps } from "@theme";
 import { useTheme } from "@shopify/restyle";
-import { Alert, FlatList } from "react-native";
+import { FlatList } from "react-native";
 import { InfoCard } from "@components/InfoCard";
 import { useNavigation } from "@react-navigation/native";
 import { useAuth } from "@providers/AuthProvider";
 import { StackHeader } from "@components/StackHeader";
-import { BottomSheetModal } from "@gorhom/bottom-sheet";
-import { useRef, useState } from "react";
 import {
-  DateTimePickerAndroid,
-  DateTimePickerEvent,
-} from "@react-native-community/datetimepicker";
-import { Ionicons } from "@expo/vector-icons";
+  BottomSheetModal,
+  BottomSheetModalProvider,
+} from "@gorhom/bottom-sheet";
+import { useRef, useState } from "react";
+
 import { CustomBottomModal } from "@components/CustomBottomModal";
 import { Lesson } from "./type";
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import config from "config";
+import LessonForm from "@components/LessonForm";
 
 export default function HomeScreen() {
   //TODO: create a teacher screen to manage lessons
   const theme = useTheme<ThemeProps>();
   const navigation = useNavigation();
-  const queryClient = useQueryClient();
   const { user, token } = useAuth().authState;
   const bottomSheetRef = useRef<BottomSheetModal>(null);
-
-  const today = new Date();
-  const [newLesson, setNewLesson] = useState<Lesson>({
-    flag: user?.plan ?? "",
-    number: undefined,
-    date: today,
-  });
+  const [isPendingMutate, setIsPendingMutate] = useState(false);
 
   const { data, isPending, isError, error } = useQuery({
     queryKey: ["lessons"],
@@ -52,69 +45,8 @@ export default function HomeScreen() {
 
   const defaultLessonNumber = data && data.length + 1;
 
-  const {
-    mutate,
-    isPending: isPendingMutate,
-    variables,
-  } = useMutation({
-    mutationFn: async (newLesson: Lesson) => {
-      const response = await fetch(config.apiBaseUrl + "/lessons", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-        body: JSON.stringify(newLesson),
-      });
-
-      const data = await response.json();
-      if (!response.ok) throw new Error(data.message, { cause: data });
-
-      return data;
-    },
-    onSuccess: () => {
-      bottomSheetRef.current?.dismiss();
-      return queryClient.invalidateQueries({ queryKey: ["lessons"] });
-    },
-    onError: (error) => {
-      Alert.alert(
-        "Algo deu errado!",
-        "Não foi possível criar a aula. Tente novamente mais tarde."
-      );
-
-      console.log(error.message, error.cause);
-    },
-  });
-
   const handleOpenBottomSheet = () => {
     if (isPending === false) bottomSheetRef.current?.present();
-  };
-
-  const handleNewLessonDateChange = (
-    e: DateTimePickerEvent,
-    selectedDate?: Date
-  ) =>
-    e.type === "set" &&
-    setNewLesson((oldLesson) => {
-      return {
-        ...oldLesson,
-        date: selectedDate ?? oldLesson.date,
-      };
-    });
-
-  const handleCreateNewLesson = () => {
-    const body = {
-      ...newLesson,
-      number: newLesson.number ?? defaultLessonNumber,
-    };
-
-    mutate(body);
-    setNewLesson({
-      flag: user?.plan ?? "",
-      number: undefined,
-      date: today,
-    });
-    bottomSheetRef.current?.dismiss();
   };
 
   const formattedDateFromISO = (isoDateString: string): string => {
@@ -233,9 +165,7 @@ export default function HomeScreen() {
                   alignItems="center"
                   gap="s"
                 >
-                  <ThemedText>
-                    Adicionando a lição {variables.number}...
-                  </ThemedText>
+                  <ThemedText>Adicionando a nova lição...</ThemedText>
                 </ThemedView>
               )
             }
@@ -248,110 +178,22 @@ export default function HomeScreen() {
             }}
           />
         )}
+      </ThemedView>
 
+      <BottomSheetModalProvider>
         <CustomBottomModal.Root ref={bottomSheetRef}>
           <CustomBottomModal.Content
             title="Nova Lição"
             subtitle="Selecione a data e o número da lição para realizar o cadastro e
                 fazer as chamadas."
           >
-            <ThemedView
-              py="xs"
-              px="m"
-              flexDirection="row"
-              justifyContent="space-between"
-              alignItems="center"
-              borderRadius={25}
-              borderWidth={1}
-              borderColor="lightgrey"
-            >
-              <ThemedText fontSize={16} style={{ color: "grey" }}>
-                Data da aula
-              </ThemedText>
-              <ThemedText
-                onPress={() =>
-                  DateTimePickerAndroid.open({
-                    value: today,
-                    minimumDate: today,
-                    onChange: handleNewLessonDateChange,
-                  })
-                }
-                fontSize={16}
-                fontWeight="600"
-              >
-                {newLesson.date.toLocaleString("pt-BR", {
-                  day: "2-digit",
-                  month: "2-digit",
-                  year: "numeric",
-                })}
-              </ThemedText>
-            </ThemedView>
-            <ThemedView
-              py="xs"
-              px="m"
-              flexDirection="row"
-              justifyContent="space-between"
-              alignItems="center"
-              borderRadius={25}
-              borderWidth={1}
-              borderColor="lightgrey"
-            >
-              <ThemedText fontSize={16} style={{ color: "grey" }}>
-                Número da lição
-              </ThemedText>
-              <ThemedView flexDirection="row" gap="s">
-                <Ionicons
-                  name="arrow-up"
-                  size={25}
-                  style={{ margin: 0 }}
-                  onPress={() =>
-                    setNewLesson((prev) => ({
-                      ...prev,
-                      number:
-                        prev.number === undefined
-                          ? defaultLessonNumber! + 1
-                          : prev.number + 1,
-                    }))
-                  }
-                />
-                <ThemedText fontSize={16} fontWeight="600">
-                  {newLesson.number ?? defaultLessonNumber}
-                </ThemedText>
-                <Ionicons
-                  name="arrow-down"
-                  size={25}
-                  style={{ margin: 0 }}
-                  onPress={() => {
-                    if (
-                      (newLesson.number === undefined &&
-                        defaultLessonNumber === 1) ||
-                      newLesson.number === 1
-                    ) {
-                      return;
-                    }
-
-                    setNewLesson((prev) => ({
-                      ...prev,
-                      number:
-                        prev.number === undefined
-                          ? defaultLessonNumber! - 1
-                          : prev.number - 1,
-                    }));
-                  }}
-                  color={
-                    (newLesson.number === undefined &&
-                      defaultLessonNumber === 1) ||
-                    newLesson.number === 1
-                      ? theme.colors.lightgrey
-                      : "black"
-                  }
-                />
-              </ThemedView>
-            </ThemedView>
+            <LessonForm
+              mutateFallback={setIsPendingMutate}
+              defaultLessonNumber={defaultLessonNumber}
+            />
           </CustomBottomModal.Content>
-          <CustomBottomModal.Action onPress={handleCreateNewLesson} />
         </CustomBottomModal.Root>
-      </ThemedView>
+      </BottomSheetModalProvider>
     </>
   );
 }
