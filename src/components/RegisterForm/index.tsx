cab451678b80e4f9a46d8d90756d7a6f943c48f9
Jonatas-lopess <jonataslopes.011@gmail.com>
2025-10-01 11:43:45 -0300

    feat(StudentScreen): Modal bug fixes

---
+++ src/components/RegisterForm/index.tsx
@@ -0,0 +1,273 @@
+import ThemedView from "@components/ThemedView";
+import ThemedText from "@components/ThemedText";
+import {
+  TextInput,
+  TouchableOpacity,
+  Pressable,
+  FlatList,
+  Alert,
+} from "react-native";
+import MaskInput, { Masks } from "react-native-mask-input";
+import {
+  DateTimePickerAndroid,
+  DateTimePickerEvent,
+} from "@react-native-community/datetimepicker";
+import { RegisterFromApp } from "@screens/StudentStack/StudentScreen/type";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { theme } from "@theme";
+import {
+  BottomSheetEventType,
+  CustomBottomModal,
+} from "@components/CustomBottomModal";
+import config from "config";
+import { useAuth } from "@providers/AuthProvider";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { BottomSheetModal } from "@gorhom/bottom-sheet";
+
+type Props = {
+  mutateFallback?: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+export default function RegisterForm({ mutateFallback }: Props) {
+  const TODAY = new Date();
+  const [inputs, setInputs] = useState<Partial<RegisterFromApp>>({});
+  const { token } = useAuth().authState;
+  const optionsSheetRef = useRef<BottomSheetModal>(null);
+  const queryClient = useQueryClient();
+
+  const handleNewRegisterDateChange = (
+    e: DateTimePickerEvent,
+    selectedDate?: Date
+  ) =>
+    e.type === "set" &&
+    setInputs((prev) => {
+      return {
+        ...prev,
+        aniversary: selectedDate?.toISOString() ?? prev?.aniversary,
+      };
+    });
+
+  const { data, isError } = useQuery({
+    queryKey: ["altclass"],
+    queryFn: async () => {
+      const res = await fetch(config.apiBaseUrl + "/classes", {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      });
+
+      const resJson = await res.json();
+      if (!res.ok) throw new Error(resJson.message, { cause: resJson.error });
+
+      return resJson;
+    },
+  });
+
+  const mutation = useMutation({
+    mutationFn: async (newData: RegisterFromApp) => {
+      const res = await fetch(config.apiBaseUrl + "/registers", {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newData),
+      });
+
+      const data = await res.json();
+      if (!res.ok) throw new Error(data.message, { cause: data.error });
+
+      return data;
+    },
+    onSuccess: () => {
+      return queryClient.invalidateQueries({ queryKey: ["register"] });
+    },
+    onError: (error) => {
+      Alert.alert(
+        "Algo deu errado!",
+        `Erro ao criar o registro. Confira sua conexão de internet e tente novamente.`
+      );
+      console.log(error.message, error.cause);
+    },
+  });
+
+  const { isPending } = mutation;
+
+  const handleOptionsSheet = useCallback((e: BottomSheetEventType) => {
+    if (e.type === "open") optionsSheetRef.current?.present();
+    if (e.type === "set") {
+      setInputs((prev) => ({
+        ...prev,
+        class: { id: e.value._id, name: e.value.name, group: e.value.group },
+      }));
+      optionsSheetRef.current?.dismiss();
+    }
+    if (e.type === "close") optionsSheetRef.current?.dismiss();
+  }, []);
+
+  function handleCreateNewRegister() {
+    if (!inputs.name || !inputs.class?.id) {
+      return Alert.alert("Atenção", "Preencha todos os campos obrigatórios.");
+    }
+
+    console.log(inputs);
+    //mutate(newRegister);
+    optionsSheetRef.current?.dismiss();
+    setInputs({});
+  }
+
+  useEffect(() => {
+    mutateFallback?.(isPending);
+  }, [isPending]);
+
+  return (
+    <>
+      <ThemedView gap="s">
+        <TextInput
+          placeholder="Nome*"
+          value={inputs.name}
+          onChangeText={(text) =>
+            setInputs((prev) => ({ ...prev, name: text }))
+          }
+          style={{
+            borderWidth: 1,
+            borderColor: theme.colors.lightgrey,
+            borderRadius: 25,
+            padding: theme.spacing.s,
+          }}
+        />
+
+        <TouchableOpacity onPress={() => handleOptionsSheet({ type: "open" })}>
+          <ThemedView
+            padding="s"
+            borderWidth={1}
+            borderColor="lightgrey"
+            borderRadius={25}
+          >
+            <ThemedText
+              style={{ color: inputs.class?.name ? "black" : "#a0a0a0" }}
+            >
+              {inputs.class && inputs.class.name !== ""
+                ? inputs.class.name
+                : "Turma*"}
+            </ThemedText>
+          </ThemedView>
+        </TouchableOpacity>
+
+        <ThemedText color="gray" fontWeight="bold" textAlign="center">
+          Informações Extras
+        </ThemedText>
+        <ThemedView flexDirection="row" gap="s">
+          <MaskInput
+            placeholder="Número de Celular"
+            value={inputs.phone}
+            onChangeText={(text) =>
+              setInputs((prev) => ({ ...prev, phone: text }))
+            }
+            mask={Masks.BRL_PHONE}
+            keyboardType="phone-pad"
+            style={{
+              flex: 2,
+              borderWidth: 1,
+              borderColor: theme.colors.lightgrey,
+              borderRadius: 25,
+              padding: theme.spacing.s,
+            }}
+          />
+          <TouchableOpacity
+            onPress={() =>
+              DateTimePickerAndroid.open({
+                value: TODAY,
+                maximumDate: TODAY,
+                onChange: handleNewRegisterDateChange,
+              })
+            }
+          >
+            <ThemedView
+              flex={1}
+              padding="s"
+              borderWidth={1}
+              borderColor="lightgrey"
+              borderRadius={25}
+            >
+              <ThemedText
+                style={{
+                  color: inputs.aniversary ? "black" : "#a0a0a0",
+                }}
+              >
+                {inputs.aniversary
+                  ? new Date(inputs.aniversary).toLocaleDateString("pt-BR")
+                  : "Data de Nascimento"}
+              </ThemedText>
+            </ThemedView>
+          </TouchableOpacity>
+        </ThemedView>
+
+        <Pressable
+          onPress={() =>
+            setInputs((prev) => ({
+              ...prev,
+              isTeacher: !prev.isTeacher,
+            }))
+          }
+        >
+          <ThemedView
+            backgroundColor={inputs.isTeacher ? "secondary" : "lightgrey"}
+            borderRadius={25}
+            py="s"
+          >
+            <ThemedText
+              textAlign="center"
+              color={inputs.isTeacher ? "white" : "gray"}
+            >
+              Criar como Professor
+            </ThemedText>
+          </ThemedView>
+        </Pressable>
+
+        <CustomBottomModal.Action onPress={handleCreateNewRegister} />
+      </ThemedView>
+
+      <CustomBottomModal.Root ref={optionsSheetRef} stackBehavior="push">
+        <CustomBottomModal.Content title="Turmas">
+          <ThemedView>
+            <FlatList
+              data={data}
+              contentContainerStyle={{ gap: theme.spacing.s }}
+              renderItem={({ item }) => (
+                <TouchableOpacity
+                  onPress={() =>
+                    handleOptionsSheet({ type: "set", value: item })
+                  }
+                >
+                  <ThemedView
+                    py="s"
+                    px="m"
+                    borderWidth={1}
+                    borderColor="lightgrey"
+                    borderRadius={25}
+                  >
+                    <ThemedText>{item.name}</ThemedText>
+                  </ThemedView>
+                </TouchableOpacity>
+              )}
+              ListEmptyComponent={() =>
+                isError ? (
+                  <ThemedText color="gray" textAlign="center">
+                    Erro ao carregar as turmas.
+                  </ThemedText>
+                ) : (
+                  <ThemedText textAlign="center" color="gray">
+                    Nenhuma turma encontrada.
+                  </ThemedText>
+                )
+              }
+            />
+          </ThemedView>
+        </CustomBottomModal.Content>
+      </CustomBottomModal.Root>
+    </>
+  );
+}
--- src/screens/StudentStack/StudentScreen/index.tsx
+++ src/screens/StudentStack/StudentScreen/index.tsx
@@ -4,46 +4,35 @@ import ThemedText from "@components/ThemedText";
 import ThemedView from "@components/ThemedView";
 import FocusAwareStatusBar from "@components/FocusAwareStatusBar";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
-import { useCallback, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import {
-  Alert,
   FlatList,
+  KeyboardAvoidingView,
+  Platform,
   Pressable,
   TextInput,
-  TouchableOpacity,
 } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { StackHeader } from "@components/StackHeader";
 import {
-  BottomSheetEventType,
-  CustomBottomModal,
-} from "@components/CustomBottomModal";
-import { BottomSheetModal } from "@gorhom/bottom-sheet";
-import {
-  DateTimePickerAndroid,
-  DateTimePickerEvent,
-} from "@react-native-community/datetimepicker";
-import MaskInput, { Masks } from "react-native-mask-input";
-import { RegisterFromApp, RegisterFromApi } from "./type";
+  BottomSheetModal,
+  BottomSheetModalProvider,
+} from "@gorhom/bottom-sheet";
+import { RegisterFromApi } from "./type";
 import { useAuth } from "@providers/AuthProvider";
 import config from "config";
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
+import { CustomBottomModal } from "@components/CustomBottomModal";
+import RegisterForm from "@components/RegisterForm";
 
 export default function StudentScreen() {
   const theme = useTheme<ThemeProps>();
   const navigation = useNavigation();
   const bottomSheetRef = useRef<BottomSheetModal>(null);
-  const optionsSheetRef = useRef<BottomSheetModal>(null);
-  const queryClient = useQueryClient();
   const [birthdayFilter, setBirthdayFilter] = useState(false);
   const [nameFilter, setNameFilter] = useState("");
-  const [newRegister, setNewRegister] = useState<RegisterFromApp>({
-    name: "",
-    isTeacher: false,
-    class: { id: "", name: "" },
-  });
   const { token, user } = useAuth().authState;
-  const TODAY = new Date();
+  const [mutateState, setMutateState] = useState(false);
 
   async function getRegisters(): Promise<RegisterFromApi[]> {
     if (user === undefined) throw new Error("User is undefined");
@@ -87,58 +76,9 @@ export default function StudentScreen() {
     queryFn: getRegisters,
   });
 
-  const { data: data_classes, isError: isErrorClass } = useQuery({
-    queryKey: ["altclass"],
-    queryFn: async () => {
-      const res = await fetch(config.apiBaseUrl + "/classes", {
-        method: "GET",
-        headers: {
-          Authorization: `Bearer ${token}`,
-          "Content-Type": "application/json",
-        },
-      });
-
-      const resJson = await res.json();
-      if (!res.ok) throw new Error(resJson.message, { cause: resJson.error });
-
-      return resJson;
-    },
-  });
-
-  const mutation = useMutation({
-    mutationFn: async (newData: RegisterFromApp) => {
-      const res = await fetch(config.apiBaseUrl + "/registers", {
-        method: "POST",
-        headers: {
-          Authorization: `Bearer ${token}`,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(newData),
-      });
-
-      const data = await res.json();
-      if (!res.ok) throw new Error(data.message, { cause: data.error });
-
-      return data;
-    },
-    onSuccess: () => {
-      cleanUp();
-      return queryClient.invalidateQueries({ queryKey: ["register"] });
-    },
-    onError: (error) => {
-      Alert.alert(
-        "Algo deu errado!",
-        `Erro ao criar o registro. Confira sua conexão de internet e tente novamente.`
-      );
-      console.log(error.message, error.cause);
-    },
-  });
-
-  const { isPending: isPendingMutate, variables, mutate } = mutation;
-
   const matchMonth = (aniversary: Date) => {
     const month = aniversary.getMonth() + 1;
-    return month === TODAY.getMonth() + 1;
+    return month === new Date().getMonth() + 1;
   };
 
   const filteredData = data
@@ -152,54 +92,12 @@ export default function StudentScreen() {
       })
     : undefined;
 
-  function cleanUp() {
-    optionsSheetRef.current?.dismiss();
-    setNewRegister({ name: "", class: { id: "", name: "" }, isTeacher: false });
-  }
-
-  const handleBottomSheet = useCallback((e: BottomSheetEventType) => {
-    if (e.type === "open") bottomSheetRef.current?.present();
-    if (e.type === "close") {
-      cleanUp();
-    }
-  }, []);
-
-  const handleOptionsSheet = useCallback((e: BottomSheetEventType) => {
-    if (e.type === "open") optionsSheetRef.current?.present();
-    if (e.type === "set") {
-      setNewRegister((prev) => ({
-        ...prev,
-        class: { id: e.value._id, name: e.value.name, group: e.value.group },
-      }));
-      optionsSheetRef.current?.dismiss();
-    }
-    if (e.type === "close") optionsSheetRef.current?.dismiss();
-  }, []);
-
-  const handleNewRegisterDateChange = (
-    e: DateTimePickerEvent,
-    selectedDate?: Date
-  ) =>
-    e.type === "set" &&
-    setNewRegister((prev) => {
-      return {
-        ...prev,
-        aniversary: selectedDate?.toISOString() ?? prev.aniversary,
-      };
-    });
-
-  function handleCreateNewRegister() {
-    if (!newRegister.name || !newRegister.class.id) {
-      return Alert.alert("Atenção", "Preencha todos os campos obrigatórios.");
-    }
-
-    mutate(newRegister);
-    cleanUp();
-  }
-
   return (
-    <>
-      <ThemedView flex={1} style={{ backgroundColor: "white" }}>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={{ flex: 1 }}
+    >
+      <ThemedView style={{ backgroundColor: "white" }}>
         <FocusAwareStatusBar style="dark" translucent />
 
         <StackHeader.Root>
@@ -215,7 +113,7 @@ export default function StudentScreen() {
             />
             <StackHeader.Action
               name="add-outline"
-              onPress={() => handleBottomSheet({ type: "open" })}
+              onPress={() => bottomSheetRef.current?.present()}
               color={theme.colors.gray}
             />
           </StackHeader.Actions>
@@ -297,16 +195,14 @@ export default function StudentScreen() {
               );
             }}
             ListFooterComponent={() =>
-              isPendingMutate && (
+              mutateState && (
                 <ThemedView
                   flexDirection="row"
                   justifyContent="center"
                   alignItems="center"
                   gap="s"
                 >
-                  <ThemedText>
-                    Adicionando o aluno {variables.name}...
-                  </ThemedText>
+                  <ThemedText>Adicionando o novo aluno...</ThemedText>
                 </ThemedView>
               )
             }
@@ -324,156 +220,18 @@ export default function StudentScreen() {
         )}
       </ThemedView>
 
-      <CustomBottomModal.Root
-        ref={bottomSheetRef}
-        onDismiss={() => handleBottomSheet({ type: "close" })}
-      >
-        <CustomBottomModal.Content title="Novo Cadastro">
-          <TextInput
-            value={newRegister.name}
-            placeholder="Nome*"
-            onChangeText={(text) =>
-              setNewRegister((prev) => ({ ...prev, name: text }))
-            }
-            style={{
-              borderWidth: 1,
-              borderColor: theme.colors.lightgrey,
-              borderRadius: 25,
-              padding: theme.spacing.s,
-            }}
-          />
-          <TouchableOpacity
-            onPress={() => handleOptionsSheet({ type: "open" })}
-          >
-            <ThemedView
-              padding="s"
-              borderWidth={1}
-              borderColor="lightgrey"
-              borderRadius={25}
-            >
-              <ThemedText
-                style={{ color: newRegister.class ? "black" : "#a0a0a0" }}
-              >
-                {newRegister.class ? newRegister.class.name : "Turma*"}
-              </ThemedText>
-            </ThemedView>
-          </TouchableOpacity>
-
-          <ThemedText color="gray" fontWeight="bold" textAlign="center">
-            Informações Extras
-          </ThemedText>
-          <ThemedView flexDirection="row" gap="s">
-            <MaskInput
-              value={newRegister.phone}
-              placeholder="Número de Celular"
-              onChangeText={(value) =>
-                setNewRegister((prev) => ({
-                  ...prev,
-                  phone: value,
-                }))
-              }
-              mask={Masks.BRL_PHONE}
-              keyboardType="phone-pad"
-              style={{
-                flex: 2,
-                borderWidth: 1,
-                borderColor: theme.colors.lightgrey,
-                borderRadius: 25,
-                padding: theme.spacing.s,
-              }}
-            />
-            <TouchableOpacity
-              onPress={() =>
-                DateTimePickerAndroid.open({
-                  value: TODAY,
-                  maximumDate: TODAY,
-                  onChange: handleNewRegisterDateChange,
-                })
-              }
-            >
-              <ThemedView
-                flex={1}
-                padding="s"
-                borderWidth={1}
-                borderColor="lightgrey"
-                borderRadius={25}
-              >
-                <ThemedText
-                  style={{
-                    color: newRegister.aniversary ? "black" : "#a0a0a0",
-                  }}
-                >
-                  {newRegister.aniversary
-                    ? new Date(newRegister.aniversary).toLocaleDateString(
-                        "pt-BR"
-                      )
-                    : "Data de Nascimento"}
-                </ThemedText>
-              </ThemedView>
-            </TouchableOpacity>
-          </ThemedView>
-
-          <Pressable
-            onPress={() =>
-              setNewRegister((prev) => ({
-                ...prev,
-                isTeacher: !prev.isTeacher,
-              }))
-            }
-          >
-            <ThemedView
-              backgroundColor={
-                newRegister.isTeacher ? "secondary" : "lightgrey"
-              }
-              borderRadius={25}
-              py="s"
-            >
-              <ThemedText
-                textAlign="center"
-                color={newRegister.isTeacher ? "white" : "gray"}
-              >
-                Criar como Professor
-              </ThemedText>
-            </ThemedView>
-          </Pressable>
-        </CustomBottomModal.Content>
-        <CustomBottomModal.Action onPress={handleCreateNewRegister} />
-      </CustomBottomModal.Root>
-
-      <CustomBottomModal.Root ref={optionsSheetRef} stackBehavior="push">
-        <CustomBottomModal.Content title="Turmas">
-          <FlatList
-            data={data_classes}
-            contentContainerStyle={{ gap: theme.spacing.s }}
-            renderItem={({ item }) => (
-              <TouchableOpacity
-                onPress={() => handleOptionsSheet({ type: "set", value: item })}
-              >
-                <ThemedView
-                  py="s"
-                  px="m"
-                  borderWidth={1}
-                  borderColor="lightgrey"
-                  borderRadius={25}
-                >
-                  <ThemedText>{item.name}</ThemedText>
-                </ThemedView>
-              </TouchableOpacity>
-            )}
-            ListEmptyComponent={() =>
-              isErrorClass ? (
-                <ThemedText color="gray" textAlign="center">
-                  Erro ao carregar as turmas.
-                </ThemedText>
-              ) : (
-                <ThemedText textAlign="center" color="gray">
-                  Nenhuma turma encontrada.
-                </ThemedText>
-              )
-            }
-          />
-        </CustomBottomModal.Content>
-      </CustomBottomModal.Root>
-    </>
+      <BottomSheetModalProvider>
+        <CustomBottomModal.Root
+          ref={bottomSheetRef}
+          onDismiss={() => {
+            bottomSheetRef.current?.dismiss();
+          }}
+        >
+          <CustomBottomModal.Content title="Novo Cadastro">
+            <RegisterForm mutateFallback={setMutateState} />
+          </CustomBottomModal.Content>
+        </CustomBottomModal.Root>
+      </BottomSheetModalProvider>
+    </KeyboardAvoidingView>
   );
 }
