5c362d71b104237064007ee8ba2b516488297a25
Jonatas-lopess <jonataslopes.011@gmail.com>
2025-10-17 12:26:21 -0300

    feat: add getRegisters funtion on register type exports

--- src/screens/RegisterStack/RegisterScreen/type.ts
+++ src/screens/RegisterStack/RegisterScreen/type.ts
@@ -1,4 +1,6 @@
 import { Never } from "@custom/types/utility_types";
+import { User } from "@providers/AuthProvider";
+import config from "config";
 
 type DefaultRegister = {
   name: string;
@@ -20,6 +22,49 @@ type RegisterFromApiExtraInfo = {
   user?: string;
   rollcalls?: { id: string; isPresent: boolean }[];
 };
+
+export async function getRegisters({
+  token,
+  user,
+}: {
+  token: string | undefined;
+  user: User | undefined;
+}): Promise<RegisterFromApi[]> {
+  if (user === undefined) throw new Error("User is undefined");
+
+  if (user.role === "teacher") {
+    const res = await fetch(
+      config.apiBaseUrl +
+        "/registers?hasUser=false&class=" +
+        user.register?.class,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    const resJson = await res.json();
+    if (!res.ok) throw new Error(resJson.message, { cause: resJson.error });
+
+    return resJson;
+  }
+
+  const res = await fetch(config.apiBaseUrl + "/registers", {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+  });
+
+  const resJson = await res.json();
+  if (!res.ok) throw new Error(resJson.message, { cause: resJson.error });
+
+  return resJson;
+}
 
 export type RegisterFromApp = DefaultRegister &
   RegisterFromAppExtraInfo &
