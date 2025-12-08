da2f3a6e12af9023b418c0b08711d4c506b3ad6c
Jonatas-lopess <jonataslopes.011@gmail.com>
2025-12-08 14:45:31 -0300

    feat(AdminAccess): add token load function (unfinished)

--- src/screens/AdminAccess/index.tsx
+++ src/screens/AdminAccess/index.tsx
@@ -2,10 +2,24 @@ import { CustomCard } from "@components/CustomCard";
 import { StackHeader } from "@components/StackHeader";
 import ThemedText from "@components/ThemedText";
 import ThemedView from "@components/ThemedView";
+import { useAuth } from "@providers/AuthProvider";
 import { useNavigation } from "@react-navigation/native";
+import { useQuery } from "@tanstack/react-query";
+import copyToClipboard from "utils/copyToClipboard";
 
 export default function AdminAccess() {
   const navigation = useNavigation();
+  const { user } = useAuth().authState;
+
+  // TODO: Substituir a função de query para buscar o token administrativo real
+  const { data, status, refetch } = useQuery({
+    queryKey: ["admin-token"],
+    queryFn: async (): Promise<string> => {
+      // Simula uma chamada de API para buscar o token administrativo
+      return "tokem-administrativo-simulado";
+    },
+    enabled: user && user.role === "owner",
+  });
 
   return (
     <>
@@ -34,7 +48,21 @@ export default function AdminAccess() {
           <CustomCard.Detail>
             Pressione sobre o campo para copiar o tokem.
           </CustomCard.Detail>
-          <CustomCard.Pressable text="tokem" onPress={() => {}} />
+          {status === "pending" && (
+            <CustomCard.Pressable text="Carregando..." onPress={() => {}} />
+          )}
+          {status === "success" && data && (
+            <CustomCard.Pressable
+              text={data}
+              onPress={() => copyToClipboard(data)}
+            />
+          )}
+          {status === "error" && (
+            <CustomCard.Pressable
+              text="Erro ao carregar o tokem... Clique para tentar novamente."
+              onPress={refetch}
+            />
+          )}
         </CustomCard.Root>
       </ThemedView>
     </>
