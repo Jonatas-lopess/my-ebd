ab9d219f35a16459781661104648a48edf2fd8a7
Jonatas-lopess <jonataslopes.011@gmail.com>
2026-01-14 18:36:43 -0300

    feat: add Toast component

--- src/components/AppRootWrapper/index.tsx
+++ src/components/AppRootWrapper/index.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { theme } from "@theme";
 import { PropsWithChildren } from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import Toast from "react-native-toast-message";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -21,6 +22,8 @@ export default function AppRootWrapper({ children }: PropsWithChildren) {
         <ThemeProvider theme={theme}>
           <GestureHandlerRootView>
             <NavigationContainer>{children}</NavigationContainer>
+
+            <Toast />
           </GestureHandlerRootView>
         </ThemeProvider>
       </AuthProvider>
